a965ccbe3b8c7b4391914688e77feed0ec7b201a
mkulz1 <mariya.kulyk1@gmail.com>
2014-11-11 14:38:05 -0500

    Fixed resize() and add() and added Exceptions

--- 01SuperArray/SuperArray.java
+++ 01SuperArray/SuperArray.java
@@ -27,23 +27,25 @@ public class SuperArray{
         data[currentLength]=o;
         currentLength ++;
         if (currentLength==data.length)
-            resize(data.length +10);
+            resize(data.length*2);
     }
     
     public void add(int index, Object o){
-        if (index >= 0) {
-            if (index < currentLength) {
+	if (index < 0 || index > data.length){
+	    throw new IndexOutOfBoundsException();
+	}else if (index >= currentLength){
+	    this.add(o);
+	}else{ 
+	    if (index < currentLength) {
                 for (int i = currentLength-1; i>index; i--) {
                     if (i==data.length)
-                        resize(data.length + 10);
+                        resize(data.length*2);
                     data[i]=data[i-1];
                 }
                 data[index] = o;
                 currentLength ++;
-            }else if (index >= currentLength) {
-                this.add(o);
-            }
-        }
+	    }
+	}
     }
 
     public Object set(int index, Object o){
@@ -58,47 +60,48 @@ public class SuperArray{
 
     private void resize(int s) {
         Object[] newData = new Object[s];
-        for (int i=0; i < data.length; i++)
+        for (int i = 0; i < s; i++)
             newData[i] = data[i];
         data=newData;
+	//	newData.currentLength = 0;
     }
-
+    
     public Object get(int index) {
-        if (index < currentLength){
-            return data[index];
+        if (index >= currentLength || index < 0){
+            throw new IndexOutOfBoundsException();
       	}else{
-	    System.out.println("ERROR: INDEX -" + index + "- IS OUT OF RANGE");
-            return null;   
+	    return data[index];  
 	}
     }
-
+    
     public Object remove(int index) {
-        if (index < currentLength && index >=0){
-            Object oldValue = data[index];
-            for (int i = index; i < currentLength - 1; i++) {
+        if (index >= currentLength || index < 0){
+	    throw new IndexOutOfBoundsException();
+        }else{
+	    Object oldValue = data[index];
+	    for (int i = index; i < currentLength - 1; i++) {
 		if (i == data.length)
-                    resize(data.length + 10);
-                data[i] = data[i+1];
-            } 
-            data[currentLength - 1] = null;
-            currentLength --;
-            return oldValue;
-        }else {
-            return null;
-        }
+		    resize(data.length + 10);
+		data[i] = data[i+1];
+	    } 
+	    data[currentLength - 1] = null;
+	    currentLength --;
+	    if(currentLength <= (data.length/4))
+	       resize(currentLength*2);
+	    return oldValue;
+	}
     }
-
+    
     public static void main(String[]args){
 	SuperArray tester = new SuperArray(5); 
 	tester.add(2);
 	tester.add(3);
 	tester.add(4);
 	tester.add(5);
-	tester.add(6);
 	//	tester.add(6);
 	//	tester.get(8);
 	tester.set(0,44);
-	tester.set(14,9);
+	//	tester.set(14,9);
 	System.out.println(tester);
 	tester.remove(0);
 	System.out.println(tester);

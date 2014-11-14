72dea8b8880c842e86d280d9c98ad32159e2c4ef
Mariya Kulyk <mariya.kulyk1@gmail.com>
2014-11-13 20:37:25 -0500

    Renamed the file and put it into the right folder

--- 02ArrayList/ArrayListMethods.java
+++ 02ArrayList/ArrayListMethods.java
@@ -1,1 +1,32 @@
-S
+imp ort java.util.*;
+public class ArrayListMethods{
+  
+      public static void collapseDuplicates(ArrayList<Integer> L){
+			for(int i = 0; i < L.size()-1; i++){	      
+	  			 if(L.get(i)==L.get(i+1)){
+		L.remove(i);
+		i--;
+	    }
+	}
+    }
+    
+    public static void main(String[]args){
+	ArrayList<Integer> L;
+	L = new ArrayList<Integer>();
+	L.add(1);
+	L.add(2);
+	L.add(6);
+	L.add(6);
+	L.add(6);
+	L.add(1);
+	L.add(1);
+	L.add(13);
+	L.add(13);
+	L.add(13);
+	System.out.println("Original Array:");
+	System.out.println(L); // See original array
+	collapseDuplicates(L);
+	System.out.println("New Collapsed Array:");
+	System.out.println(L); // See new array
+    }
+}

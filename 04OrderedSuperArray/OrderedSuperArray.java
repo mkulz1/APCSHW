e9aecee2bd9ec1a3df847fe9f1ad9f8e6fe36537
mkulz1 <mariya.kulyk1@gmail.com>
2014-12-01 13:18:37 -0500

    Trying to make the regular add work in subclass.

--- 04OrderedSuperArray/OrderedSuperArray.java
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -1,7 +1,25 @@
 public class OrderedSuperArray extends SuperArray{
+    int currentLength = 0;
 
+    public OrderedSuperArray(){
+	super();
+    }
+    public OrderedSuperArray(int size){
+	super(size);
+    }
 
+    public void add(String o){
+	super.add(o);
+    }
 
-
-
+    public static void main(String[] args){
+	OrderedSuperArray byes = new OrderedSuperArray(4);
+	byes.add("bye");
+	byes.add("good day");
+	byes.add("good bye");
+	byes.add("adios");
+	//	byes.add("see ya");
+	System.out.println("Inside the array...");
+	System.out.println(byes);
+    }
 }

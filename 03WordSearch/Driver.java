79c941ca0c3f5de52df05b41298f4efde5df5c5e
mkulz1 <mariya.kulyk1@gmail.com>
2014-12-01 21:34:21 -0500

    Make args conditions and inputs.

--- 03WordSearch/Driver.java
+++ 03WordSearch/Driver.java
@@ -9,8 +9,26 @@ public class Driver{
 	boolean isSeed = false;
 
 	if (args.length == 2){
+	    rows = args[0];
+	    columns = args[1];
+	}else if(args.length == 3){
+	    rows = args[0];
+	    columns = args[1];
+	    isSeed = true;
+	}else if(args.length == 4){
+	    rows = args[0];
+	    columns = args[1];
+	    isSeed = true;
+	    if (args[3].eguals("1")){
+		fillRandomLetters = false;
+	    }
+	}else{
+	    System.out.println("To generate a WordSearch:");
+	    System.out.println("[rows] & [columns] are your chosen dimensions of the puzzle");
+	    System.out.println("[RandomSeed] is a random optional number");
+	    System.out.println("[ShowSolution?]--> 1 if you want the answers. Leave blank if you do not.");
+	    System.out.println("java Driver [rows] [columns] [RandomSeed] [ShowSolution?]");
 	}
-
 
 	WordGrid data = new WordGrid(rows, columns);
 	if(isSeed){

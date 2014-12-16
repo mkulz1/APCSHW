cb45cb9772f86c11073d6e15d304c68d490032ae
mkulz1 <mariya.kulyk1@gmail.com>
2014-12-16 13:20:34 -0500

    Started to code bubble() sort

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -1,5 +1,32 @@
 public class Sorts{
-
-
-
+    public static void main(String[] args){
+	
+	int[] data;
+	
+	public static void bubble(int[] c){
+	    int holder;
+	    for(int i = 0; i < c.length; i++){
+		if(c[i] > c[i+1]){
+		    holder = c[i+1];
+		    c[i] = c[i+1];
+		    c[i+1] = holder;
+		}
+	    }
+	}
+	
+	public static void insertion(int[] c){
+	    
+	}
+	
+	public static void selection(int[] c){
+	    int small;
+	    int holder;
+	    for(int i = 0; i < size(); i++){
+	
+	    }
+	}    
+    
+    public static void main(String[] args){
+ 
+    }
 }

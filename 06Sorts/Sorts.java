2980c550dcaf0cd605157fe9c1d6e912f955c990
mkulz1 <mariya.kulyk1@gmail.com>
2014-12-17 10:58:26 -0500

    bubble sort. still needs to be tested

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -1,13 +1,10 @@
 public class Sorts{
-    public static void main(String[] args){
-	
-	int[] data;
-	
+   	
 	public static void bubble(int[] c){
 	    int holder;
 	    for(int i = 0; i < c.length; i++){
 		if(c[i] > c[i+1]){
-		    holder = c[i+1];
+		    holder = c[i];
 		    c[i] = c[i+1];
 		    c[i+1] = holder;
 		}
@@ -18,15 +15,19 @@ public class Sorts{
 	    
 	}
 	
-	public static void selection(int[] c){
+    /*	public static void selection(int[] c){
 	    int small;
 	    int holder;
 	    for(int i = 0; i < size(); i++){
-	
+		if( c[i] < c[i+1]){
+
+		}
 	    }
-	}    
+	    } */   
     
     public static void main(String[] args){
- 
+	int[] data = new int[5];
+	data = {6,7,2,5,4};
+	System.out.println(data);
     }
 }

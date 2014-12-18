f4937c31f204476fc014c328e0db13db63d1c336
mkulz1 <mariya.kulyk1@gmail.com>
2014-12-17 21:54:26 -0500

    Woohoo selection works too

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -1,20 +1,20 @@
 public class Sorts{
-   	
-	public static void bubble(int[] c){
-	    int holder;
-	    for(int x = c.length - 1; x > 0; x--){
-		for(int i = 0; i < x; i++){
-		    if(c[i] > c[i+1]){
-			holder = c[i];
-			c[i] = c[i+1];
-			c[i+1] = holder;
-		    }
+    
+    public static void bubble(int[] c){
+	int holder;
+	for(int x = c.length - 1; x > 0; x--){
+	    for(int i = 0; i < x; i++){
+		if(c[i] > c[i+1]){
+		    holder = c[i];
+		    c[i] = c[i+1];
+		    c[i+1] = holder;
 		}
 	    }
 	}
-	
-	public static void insertion(int[] c){
-	    for(int i = 0; i < c.length; i++){
+    }
+    
+    public static void insertion(int[] c){
+	for(int i = 0; i < c.length; i++){
 		int n = c[i];
 		int x = i;
 		while( x > 0 && c[x - 1] > n){
@@ -22,8 +22,8 @@ public class Sorts{
 		    x--;
 		}
 		c[x] = n;
-	    }  
-	}
+	}  
+    }
     
     // Needs toString
     public static String toString(int[] c){
@@ -36,14 +36,24 @@ public class Sorts{
     }
     
     public static void selection(int[] c){
-	
+	for(int i = 0; i < c.length-1; i++){
+	    int min = i;
+	    for(int x = i+ 1; x < c.length; x++){
+		if(c[x] < c[min])
+		    min = x;
+	    }
+	    int temp = c[i];
+	    c[i] = c[min];
+	    c[min] = temp;
+	}
     }  
     
     public static void main(String[] args){
 	int[] data = new int[]{4,6,3,2,5,2};
 	System.out.println(toString(data));
 	//	bubble(data);
-	insertion(data);
+	//	insertion(data);
+	selection(data);
 	System.out.println(toString(data));
     }
 }

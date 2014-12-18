6d2ea53f5d7dbc8991805c3e2adc83a325702f89
mkulz1 <mariya.kulyk1@gmail.com>
2014-12-17 21:25:34 -0500

    Fixing the bubble sort, which doesn't seem to work

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -2,11 +2,13 @@ public class Sorts{
    	
 	public static void bubble(int[] c){
 	    int holder;
-	    for(int i = 0; i < c.length; i++){
-		if(c[i] > c[i+1]){
-		    holder = c[i];
-		    c[i] = c[i+1];
-		    c[i+1] = holder;
+	    for(int x = 0; x < c.length-1; x++){
+		for(int i = 0; i < x - 1; i++){ 
+		    if(c[i] < c[i+1]){
+			holder = c[i];
+			c[i] = c[i+1];
+			c[i+1] = holder;
+		    }
 		}
 	    }
 	}
@@ -14,22 +16,25 @@ public class Sorts{
 	public static void insertion(int[] c){
 	    
 	}
-	
-    /*	public static void selection(int[] c){
-	    int small;
-	    int holder;
-	    for(int i = 0; i < size(); i++){
-		if( c[i] < c[i+1]){
 
-		}
-	    }
-	    } */   
+    // Needs toString
+    public static String toString(int[] c){
+	String result = "{ "; 
+	for (int i = 0; i < c.length; i++){
+	    result += c[i] + " ";
+	}
+	result += "}";
+	return result;
+    }
+    
+    public static void selection(int[] c){
+
+    }  
     
     public static void main(String[] args){
-	int[] data = new int[5];
-	for (int i = 5; i > 0; i --){
-	    data[5 - i] = i;
-	}
-	System.out.println(data);
+	int[] data = new int[]{4,6,3,2,5,2};
+	System.out.println(toString(data));
+	bubble(data);
+	System.out.println(toString(data));
     }
 }
